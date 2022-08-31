7c20694b6c3412556eeb6ccf5ef3b48c2b09125f
DK <dhananjaykudche256@gmail.com>
2022-08-31 22:47:07 +0530

    try

---
+++ src/main/java/com/enotes/entity/Notes.java
@@ -0,0 +1,57 @@
+package com.enotes.entity;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.FetchType;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.JoinColumn;
+import javax.persistence.ManyToOne;
+import javax.persistence.Table;
+
+import org.hibernate.annotations.OnDelete;
+import org.hibernate.annotations.OnDeleteAction;
+
+import lombok.Data;
+
+@Data
+@Entity
+@Table(name = "notes")
+public class Notes extends Audit {
+
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	private int id;
+	public int getId() {
+		return id;
+	}
+
+	public void setId(int id) {
+		this.id = id;
+	}
+
+	@Column(nullable = false)
+	private String title;
+
+	@Column(nullable = false)
+	private String content;
+
+	@ManyToOne(fetch = FetchType.LAZY)
+	@JoinColumn(name = "user_id", nullable = false)
+	@OnDelete(action = OnDeleteAction.CASCADE)
+	private UserDtls userDtls;
+
+	public UserDtls getUserDtls() {
+		return userDtls;
+	}
+
+	public void setUserDtls(UserDtls userDtls) {
+		this.userDtls = userDtls;
+	}
+
+	public Notes() {
+		// TODO Auto-generated constructor stub
+	}
+
+}
